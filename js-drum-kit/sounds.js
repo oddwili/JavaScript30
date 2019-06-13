5204a877fcf0cecec501f394db0577739f1c3916
Audrey Magwili <audreymagwili@Audreys-MacBook-Air.local>
2019-06-13 12:35:48 -0700

    animation function

--- js-drum-kit/sounds.js
+++ js-drum-kit/sounds.js
@@ -21,3 +21,33 @@ function playAudio(e) {
     audio.currentTime = 0;
     audio.play();
 }
+
+function animate(e) {
+    let key;
+
+    if (e.type == 'click') {
+        if (e.target.classList == 'key') {
+            key = document.querySelector(`div[data-key="${e.target.dataset.key}"]`);
+        } else {
+            return;
+        }
+    } else {
+        key = document.querySelector(`div[data-key="${e.key.toUpperCase()}"]`);
+    }
+
+    key.classList.add('playing');
+    key.addEventListener('transitionstart', () => {
+        
+    })
+    // get element
+    // add 'playing' class
+    // remove when animation is over
+}
+
+function playSoundAndAnimate(e) {
+    playAudio(e);
+    animate(e);
+}
+
+window.addEventListener('keydown', (e) => playSoundAndAnimate(e));
+keys.addEventListener('click', (e) => playSoundAndAnimate(e));
