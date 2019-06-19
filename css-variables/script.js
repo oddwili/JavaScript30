3fbf6f5aa4ab5327788c5f485fc19b32bb9f3acc
Audrey Magwili <audreymagwili@Audreys-MacBook-Air.local>
2019-06-19 15:42:57 -0700

    remove logs

--- css-variables/script.js
+++ css-variables/script.js
@@ -2,26 +2,30 @@ const spacing = document.querySelector('#spacing');
 const blur = document.querySelector('#blur');
 const base = document.querySelector('#base');
 const controls = document.querySelector('.controls');
+const poster = document.querySelector('#poster');
 
 
 function handleUpdate(e) {
+    console.log(e.buttons);
     const suffix = e.target.dataset;
-    const el = e.target
+    const el = e.target;
 
-    console.log(el.name);
     switch (el.name) {
         case 'spacing':
-            document.documentElement.style.setProperty(`--${el.name}`, `${el.value}${suffix.sizing}`);
+            poster.style.setProperty(`--${el.name}`, `${el.value}${suffix.sizing}`);
             break;
         case 'blur':
-            document.documentElement.style.setProperty(`--${el.name}`, `${el.value}${suffix.sizing}`);
+            poster.style.setProperty(`--${el.name}`, `${el.value}${suffix.sizing}`);
             break;
         case 'base':
-            document.documentElement.style.setProperty(`--${el.name}`, `${el.value}`);
+            poster.style.setProperty(`--${el.name}`, `${el.value}`);
             break;
         default:
     }
 }
-controls.addEventListener('change', (e) => handleUpdate(e));
-
-// also on mouse press?
+// controls.addEventListener('change', (e) => handleUpdate(e));
+controls.addEventListener('mouseover', (e) => {
+    // while (e.buttons == 1) {
+        handleUpdate(e)
+    // }
+});
